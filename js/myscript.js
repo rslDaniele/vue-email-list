8f42deef502aeb49487926baddb3f9e2aa9d2628
Daniele <danlup94@gmail.com>
2020-11-30 19:43:05 +0100

    Aggiunta bottone v-on:click
    esercizio completato

--- js/myscript.js
+++ js/myscript.js
@@ -2,19 +2,35 @@ var app = new Vue({
   el: "#app",
   data: {
     titolo: "Generatore email casuali",
-    mail: '',
     listaMail: []
   },
 
-  mounted: function () {
-    for (var i = 0; i < 10; i++) {
-      axios
-      .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(risposta => {
-        console.log(risposta.data.response);
-        this.listaMail.push(risposta.data.response);
-        console.log(listaMail);
-      })
+  // FUNZIONE MOUNTED
+  // mounted: function () {
+  //   for (var i = 0; i < 10; i++) {
+  //     axios
+  //     .get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //     .then(risposta => {
+  //       console.log(risposta.data.response);
+  //       this.listaMail.push(risposta.data.response)
+  //     })
+  //   }
+  // }
+
+  // FUNZIONE CON METHOD E BUTTON
+
+  methods: {
+    genera: function () {
+      for (var i = 0; i < 10; i++) {
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(risposta => {
+          console.log(risposta.data.response);
+          this.listaMail.push(risposta.data.response)
+        })
+      }
     }
   }
+
+
 })
